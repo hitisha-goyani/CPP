38c53add0627f0b1debec142635527399a6f37a3
hitisha-goyani <goyanihitisha@gmail.com>
2024-11-18 15:52:53 +0530

    day 1

---
+++ day-1/operator.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+ int main(){
+
+
+    int a = 50;
+    int b = 100;
+
+    cout << "addition of a & b : "<< a+b <<endl;
+    cout << "substraction of a & b : "<< a-b <<endl;
+    cout << "multiplication of a & b : "<< a*b <<endl;
+    cout << "divion of a & b : "<< a/b <<endl;
+    cout << "module of a & b : "<< a%b <<endl;
+ }
---
+++ day-1/pattern.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    cout<<"* * * * * *"<<endl;
+    cout<<"*  o   o  *"<<endl;
+    cout<<"*    ^    *"<<endl;
+    cout<<"*   ===   *"<<endl;
+    cout<<"*         *"<<endl;
+    cout<<"* * * * * *"<<endl;
+
+
+
+    cout<<"*         *"<<endl;
+    cout<<"*         *"<<endl;
+    cout<<"*         *"<<endl;
+    cout<<"* * * * * *"<<endl;
+    cout<<"*         *"<<endl;
+    cout<<"*         *"<<endl;
+    cout<<"*         *"<<endl;
+
+
+}
