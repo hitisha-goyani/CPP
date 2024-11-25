b7ec76bdfb4778513e71a2c3783baf2534e3d11f
hitisha-goyani <goyanihitisha@gmail.com>
2024-11-25 16:01:29 +0530

    day-6

---
+++ day-6/factorial.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+    int num;
+    cout<<"num";
+    cin>>num;
+
+    int factorial = 1;
+
+    for(int i=num;i>=1;i--){
+        factorial *=i;
+    }
+    cout<<"factorial :"<<factorial;
+    return 0;
+}
