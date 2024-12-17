22afa1a9d6eaad8479b531276e4dcefc04f2aac2
hitisha-goyani <goyanihitisha@gmail.com>
2024-12-17 17:35:57 +0530

    day-20

--- day-7/pattern_space.cpp
+++ day-7/pattern_space.cpp
@@ -19,18 +19,18 @@ int main(){
 
     // second pattern
 
-     for(int i=1;i<=5;i++){
-
-        for(int k=i;k<=5;k++){
-            cout<<" ";
-        }
-        for(int j=1;j<=i;j++){
-             cout<<"*"<<" ";
-
-        }
-         cout<<endl;
-    }
-    cout<<endl;
+	     for(int i=1;i<=5;i++){
+	
+	        for(int k=i;k<=5;k++){
+	            cout<<" ";
+	        }
+	        for(int j=1;j<=i;j++){
+	             cout<<"*"<<" ";
+	
+	        }
+	         cout<<endl;
+	    }
+	    cout<<endl;
 
     // third pattern
      for(int i=1;i<=5;i++){
