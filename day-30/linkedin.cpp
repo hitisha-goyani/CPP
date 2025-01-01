2e2bd32c0e90a2fec7c5ce10d712bc7c254911a9
hitisha-goyani <goyanihitisha@gmail.com>
2025-01-01 17:31:18 +0530

    day-30

---
+++ day-30/linkedin.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+
+using namespace std;
+
+class Node{
+
+    public:
+    int data;
+    Node *next;
+
+
+};
+
+int main()
+{
+
+
+    Node *HEAD,*current;
+
+    HEAD = new Node(); 
+    HEAD->data=10;
+    HEAD->next=NULL;
+
+    Node *ptr=HEAD;
+
+    current = new Node();
+    current->data=20;
+    current->next=NULL;
+    HEAD->next=current;
+
+    current = new Node();
+    current->data=30;
+    current->next=NULL;
+    HEAD->next->next=current;
+
+    current = new Node();
+    current->data=40;
+    current->next=NULL;
+    HEAD->next->next->next=current;
+
+
+    // cout<<"HEAD: "<<HEAD->data<<" address:"<<HEAD->next<<endl;
+    // cout<<"HEAD: "<<HEAD->next->data<<" address:"<<HEAD->next<<endl;
+    // cout<<"HEAD: "<<HEAD->next->next->data<<" address:"<<HEAD->next->next<<endl;
+    // cout<<"HEAD: "<<HEAD->next->next->next->data<<" address:"<<HEAD->next->next->next<<endl;
+
+    while(ptr !=0){
+        cout<<"data: "<<ptr->data<<" address: "<<ptr->next<<endl;
+        ptr = ptr->next;
+    }
+
+
+
+
+
+    return 0;
+}
